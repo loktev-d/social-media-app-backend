4241ef69ff7d19af4652403bacc7035472391789
loktev-d <lokt.daniil@gmail.com>
2021-07-29 11:36:29 +0300

    Extend request limit

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(json({ limit: '50mb' }));
+  app.use(urlencoded({ limit: '50mb', extended: true }));
   const port = app.get(ConfigService).get<number>('PORT');
   await app.listen(port ?? 3000);
 }
